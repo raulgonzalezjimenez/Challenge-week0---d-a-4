e8977728f7642b6a4a419d0e07b8ad0f9ba0302f
raulgonzalez <raulgonji@gmail.com>
2024-02-15 22:26:47 +0100

    Update exercise  3

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ export const palandrino = (y) => {
 
   return y === reverse1;
 };
+
+//3. Crea una función que cuente las vocales que contiene una palabra dada por parámetros.
+export const vowels = (x) => {
+  let vocales = 0;
+  for (let i = 0; i < x.length; i++) {
+    if (
+      x[i] === 'a' ||
+      x[i] === 'e' ||
+      x[i] === 'i' ||
+      x[i] === 'o' ||
+      x[i] === 'u'
+    ) {
+      vocales = vocales + 1;
+    }
+  }
+  return vocales;
+};
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import { cortar, palandrino } from './index.js';
+import { cortar, palandrino, vowels } from './index.js';
 
 console.log(cortar('caramelo'));
 
 console.log(palandrino('rapar'));
+
+console.log(vowels('adoptar'));
